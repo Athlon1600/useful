b75b89b106df418f9514389c1fa2ebb46bb6cc8e
Athlon1600 <Athlon2400@gmail.com>
2020-04-04 12:28:09 -0500

    Create deploy-laravel.sh

---
+++ deploy-laravel.sh
@@ -0,0 +1,15 @@
+cd /var/www/newsite.com
+git pull origin master
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+echo "" | sudo -S service php7.3-fpm reload
+
+if [ -f artisan ]
+then
+    php artisan migrate --force
+fi
+
+rm -f bootstrap/cache/config.php
+php artisan config:cache
+
+npm ci
+npm run production
