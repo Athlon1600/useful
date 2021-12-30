150d65182eb13506d5dd97109eac413133eae835
Athlon1600 <Athlon2400@gmail.com>
2021-12-29 21:11:16 -0600

    Update install.sh

--- squid/install.sh
+++ squid/install.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt -y install squid
+CENTOS=0
+
+if [ -f "/etc/centos-release" ]; then
+    CENTOS=1
+fi
+
+if [ $CENTOS -eq 1 ]; then
+    sudo yum check-update
+    sudo yum -y install squid wget
+else
+    sudo apt-get update
+    sudo apt -y install squid
+fi
 
 wget https://raw.githubusercontent.com/Athlon1600/useful/master/squid/squid-open.conf -O /etc/squid/squid.conf
 squid -k reconfigure
